e3ef9a2e466858439b974d66fc1077f8136d6d13
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-26 15:55:19 +0300

    Refactor index creation: removed duplicate indexes and optimized index definitions for Loans, Books, and Borrowers tables

--- Indexes/Indexes.sql
+++ Indexes/Indexes.sql
@@ -1,12 +1,20 @@
-CREATE UNIQUE INDEX idx_Books_ISBN ON Books(ISBN);
-CREATE INDEX idx_Books_Genre ON Books(Genre);
-CREATE INDEX idx_Books_CurrentStatus ON Books(Current_Status);
+CREATE INDEX idx_Loan_BorrowerID ON Loans (BorrowerID);
+CREATE INDEX idx_Loan_LoanID ON Loans (LoanID);
+CREATE INDEX idx_Loan_BookID ON Loans (BookID);
+CREATE INDEX idx_Loan_DateBorrowed ON Loans (Date_Borrowed);
+CREATE INDEX idx_Loan_BorrowerID_DateReturned ON Loans (BorrowerID, Date_Returned);
+CREATE INDEX idx_Loan_DateReturned_DueDate ON Loans (Date_Returned, Due_Date); 
+CREATE INDEX idx_Loan_DateBorrowed ON Loans (Date_Borrowed);
+CREATE NONCLUSTERED INDEX idx_Loans_DateReturned_BorrowerID ON Loans (Date_Returned) INCLUDE (BorrowerID);
 
-CREATE UNIQUE INDEX idx_Borrowers_Email ON Borrowers(Email);
-CREATE INDEX idx_Borrowers_DateOfBirth ON Borrowers(Date_Of_Birth);
+CREATE INDEX idx_BookID ON Books (BookID);
+CREATE INDEX idx_Book_Author ON Books (Author);
+CREATE INDEX idx_Book_CurrentStatus ON Books (Current_Status);
 
-CREATE INDEX idx_Loans_BookID ON Loans(BookID);
-CREATE INDEX idx_Loans_BorrowerID ON Loans(BorrowerID);
-CREATE INDEX idx_Loans_DateBorrowed ON Loans(Date_Borrowed);
-CREATE INDEX idx_Loans_DueDate ON Loans(Due_Date);
-CREATE INDEX idx_Loans_DateReturned ON Loans(Date_Returned);
+CREATE INDEX idx_BorrowerID ON Borrowers (BorrowerID);
+CREATE INDEX idx_Borrower_Email ON Borrowers (Email);
+
+
+
+
+
